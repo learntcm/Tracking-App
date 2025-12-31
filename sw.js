f7d20c99c5d92f4450318e2fffbf3b2e894cdbf5
learntcm <63395778+learntcm@users.noreply.github.com>
2025-12-31 13:07:17 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,24 @@
-const CACHE_NAME = 'tracker-cache-v1';
+const CACHE_NAME = 'tracker-cache-v2';
+
 const urlsToCache = [
-    './',
-    './index.html',
-    './app.js',
-    './manifest.json'
+  './',
+  './index.html',
+  './app.js',
+  './manifest.json',
+  './icon.png',     // your PNG logo
+  './ad1.jpg',
+  './ad2.jpg',
+  './ad3.jpg'
 ];
 
 self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
-    );
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
 });
 
 self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => response || fetch(event.request))
-    );
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
 });
